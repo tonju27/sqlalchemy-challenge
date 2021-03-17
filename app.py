cad116ecde752761fe6e97e165a2b1c1cb761930
Anthony Njuguna <tonju2@yahoo.com>
2021-03-17 05:13:22 -0500

    11th version

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Adopted from class material
+
+#################################################
+# Database Setup. 
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
